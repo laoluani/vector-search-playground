2668a4d0f8f2e8852879e025a8f3d6bd321d166a
Matt993 <matt_993@hotmail.co.uk>
2025-07-03 10:48:54 +0100

    add docstring

--- distance.py
+++ distance.py
@@ -1,4 +1,5 @@
 import numpy as np
 
 def cos_similarity(a: np.ndarray, b: np.ndarray) -> float:
+    """Calculates the cosine similarity between two vectors."""
     return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
