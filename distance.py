3abb64c385295fc5c23daa4425b12bd34980f691
Matt993 <matt_993@hotmail.co.uk>
2025-07-03 10:08:50 +0100

    return type

--- distance.py
+++ distance.py
@@ -1,4 +1,4 @@
 import numpy as np
 
-def cos_similarity(a: np.ndarray, b: np.ndarray):
+def cos_similarity(a: np.ndarray, b: np.ndarray) -> float:
     return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
