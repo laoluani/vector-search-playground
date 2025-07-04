063caf7bd1def88957222d7b6efc46f513355e72
Laolu Animashaun <laolu.animashaun@iproov.com>
2025-07-04 06:22:50 +0100

    parse embeddings file

---
+++ benchmark/data/fasttext/parse.py
@@ -0,0 +1,35 @@
+import io
+
+import numpy as np
+
+
+def load_vectors(path: str, size: int) -> tuple[list[str], np.ndarray]:
+    fin = io.open(path, 'r', encoding='utf-8', newline='\n', errors='ignore')
+    n, d = map(int, fin.readline().split())
+
+    size = min(n, size)
+
+    vectors = np.empty((size, d), dtype=np.float32)
+    words = []
+
+    for i, line in enumerate(fin):
+        if i == n: 
+            break
+
+        tokens = line.rstrip().split(' ')
+        words.append(tokens[0])
+        vectors[i] =  map(float, tokens[1:])
+    return words, vectors
+
+embeddings_file_name = "embeddings.npy"
+words_file_name = "words.txt"
+fasttext_vectors_path = ""
+size = 100
+
+words, vectors = load_vectors(fasttext_vectors_path, 100)
+
+with words_file_name.open("w") as f:
+    for word in words:
+        f.write(word + "\n")
+    
+np.save(embeddings_file_name, vectors)
