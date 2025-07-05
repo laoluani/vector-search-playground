d31b9deed4bcc6620dd17c3584528291948a2009
Laolu Animashaun <laolu.animashaun@iproov.com>
2025-07-05 15:34:43 +0100

    minor clean up

--- benchmark/data/fasttext/download.sh
+++ benchmark/data/fasttext/download.sh
@@ -4,16 +4,12 @@ set -e
 # 1 million word vectors trained on Wikipedia 2017, UMBC webbase corpus and statmt.org news dataset (16B tokens).
 URL = https://dl.fbaipublicfiles.com/fasttext/vectors-english/wiki-news-300d-1M.vec.zip
 FASTTEXT_FILENAME=fasttext.vec
-OUTPUT_DIR = ""
 
 if [ ! -f $FASTTEXT_FILENAME ]
 then
     curl $URL --output $FASTTEXT_FILENAME.zip
     unzip -d $FASTTEXT_FILENAME.zip
 fi
-
-mkdir $OUTPUT_DIR
-
 
 # Run the parsing script
 
