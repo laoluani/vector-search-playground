1f665169b91d73cd6b0fd0f2f1bf7bea0668c778
Laolu Animashaun <laolu.animashaun@iproov.com>
2025-07-07 01:48:52 +0100

    fasttext dataset download works

--- benchmark/data/fasttext/download.sh
+++ benchmark/data/fasttext/download.sh
@@ -2,16 +2,18 @@ set -e
 
 
 # 1 million word vectors trained on Wikipedia 2017, UMBC webbase corpus and statmt.org news dataset (16B tokens).
-URL = https://dl.fbaipublicfiles.com/fasttext/vectors-english/wiki-news-300d-1M.vec.zip
-FASTTEXT_FILENAME=benchmark/data/fastext/fasttext.vec
+URL=https://dl.fbaipublicfiles.com/fasttext/vectors-english/wiki-news-300d-1M.vec.zip
+FASTTEXT_FILENAME=benchmark/data/fasttext/fasttext.vec
 
 if [ ! -f $FASTTEXT_FILENAME ]
 then
     curl $URL --output $FASTTEXT_FILENAME.zip
-    unzip -d $FASTTEXT_FILENAME.zip
+    unzip $FASTTEXT_FILENAME.zip -d benchmark/data/fasttext/
+    mv benchmark/data/fasttext/wiki-news-300d-1M.vec $FASTTEXT_FILENAME
 fi
 
 # Run the parsing script
 
-python parse.py
+
+python benchmark/data/fasttext/parse.py
 
--- benchmark/data/fasttext/parse.py
+++ benchmark/data/fasttext/parse.py
@@ -3,6 +3,7 @@ import io
 import numpy as np
 import tqdm
 
+from pathlib import Path
 
 def load_vectors(path: str, size: int) -> tuple[list[str], np.ndarray]:
     fin = io.open(path, 'r', encoding='utf-8', newline='\n', errors='ignore')
@@ -19,17 +20,18 @@ def load_vectors(path: str, size: int) -> tuple[list[str], np.ndarray]:
 
         tokens = line.rstrip().split(' ')
         words.append(tokens[0])
-        vectors[i] =  map(float, tokens[1:])
+        vectors[i] = list(map(float, tokens[1:]))
+    
     return words, vectors
 
-embeddings_file_name = "benchmark/data/fastext/embeddings.npy"
-words_file_name = "benchmark/data/fastext/words.txt"
-fasttext_vectors_path = "benchmark/data/fastext/fasttext.vec"
+embeddings_file_name = "benchmark/data/fasttext/embeddings.npy"
+words_file_name = "benchmark/data/fasttext/words.txt"
+fasttext_vectors_path = "benchmark/data/fasttext/fasttext.vec"
 size = 100
 
 words, vectors = load_vectors(fasttext_vectors_path, 100)
 
-with words_file_name.open("w") as f:
+with Path(words_file_name).open("w") as f:
     for word in words:
         f.write(word + "\n")
     
