89637ffddf6958867e0e423f5d6a7bd36d66e641
Laolu Animashaun <laolu.animashaun@iproov.com>
2025-07-07 01:19:18 +0100

    minor improvements

--- benchmark/data/fasttext/download.sh
+++ benchmark/data/fasttext/download.sh
@@ -3,7 +3,7 @@ set -e
 
 # 1 million word vectors trained on Wikipedia 2017, UMBC webbase corpus and statmt.org news dataset (16B tokens).
 URL = https://dl.fbaipublicfiles.com/fasttext/vectors-english/wiki-news-300d-1M.vec.zip
-FASTTEXT_FILENAME=fasttext.vec
+FASTTEXT_FILENAME=benchmark/data/fastext/fasttext.vec
 
 if [ ! -f $FASTTEXT_FILENAME ]
 then
--- benchmark/data/fasttext/parse.py
+++ benchmark/data/fasttext/parse.py
@@ -1,6 +1,7 @@
 import io
 
 import numpy as np
+import tqdm
 
 
 def load_vectors(path: str, size: int) -> tuple[list[str], np.ndarray]:
@@ -12,8 +13,8 @@ def load_vectors(path: str, size: int) -> tuple[list[str], np.ndarray]:
     vectors = np.empty((size, d), dtype=np.float32)
     words = []
 
-    for i, line in enumerate(fin):
-        if i == n: 
+    for i, line in tqdm.tqdm(enumerate(fin)):
+        if i == size: 
             break
 
         tokens = line.rstrip().split(' ')
@@ -21,9 +22,9 @@ def load_vectors(path: str, size: int) -> tuple[list[str], np.ndarray]:
         vectors[i] =  map(float, tokens[1:])
     return words, vectors
 
-embeddings_file_name = "embeddings.npy"
-words_file_name = "words.txt"
-fasttext_vectors_path = "fasttext.vec"
+embeddings_file_name = "benchmark/data/fastext/embeddings.npy"
+words_file_name = "benchmark/data/fastext/words.txt"
+fasttext_vectors_path = "benchmark/data/fastext/fasttext.vec"
 size = 100
 
 words, vectors = load_vectors(fasttext_vectors_path, 100)
